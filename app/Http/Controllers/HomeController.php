ce87f07357c9a424715b802a89c26063f431290d
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-23 06:55:37 +0100

    adding method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+
+
+    public function change(Request $request)
+    {
+        App::setLocale($request->lang);
+        session()->put('locale', $request->lang);
+
+        return redirect()->back();
+    }
 }
